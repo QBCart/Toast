3f61cc3420a3bb6bd57b2cd1899b72ddb35f56cf
Mshea424 <61366103+Mshea424@users.noreply.github.com>
2021-03-09 14:42:49 -0500

    removed unnecessary parameter

--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,7 @@ import React from 'https://cdn.skypack.dev/pin/react@v17.0.1-tOtrZxBRexARODgO0jl
 import { render } from 'https://cdn.skypack.dev/pin/react-dom@v17.0.1-DtIXT56q6U8PbgLMrBhE/min/react-dom.js';
 import Toast from './toast'
 
-const mountToast = (cartAPI?: string) => {
+const mountToast = () => {
     const toast = document.getElementById('qbc-toast');
     render(
       <Toast
