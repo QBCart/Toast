c30cf87c9a01516c3297469bd079702fdbb22873
Mshea424 <61366103+Mshea424@users.noreply.github.com>
2021-03-09 16:44:56 -0500

    exported showToast method

--- src/index.tsx
+++ src/index.tsx
@@ -6,19 +6,31 @@
 
 // React import is needed by snowpack even though typescript suggests otherwise
 // @ts-ignore
-import React from 'https://cdn.skypack.dev/pin/react@v17.0.1-tOtrZxBRexARODgO0jli/min/react.js';
-import { render } from 'https://cdn.skypack.dev/pin/react-dom@v17.0.1-DtIXT56q6U8PbgLMrBhE/min/react-dom.js';
-import Toast from './toast'
+import React from "https://cdn.skypack.dev/pin/react@v17.0.1-tOtrZxBRexARODgO0jli/min/react.js";
+import { render } from "https://cdn.skypack.dev/pin/react-dom@v17.0.1-DtIXT56q6U8PbgLMrBhE/min/react-dom.js";
+import Toast from "./toast";
+
+const id = "qbc-toast";
 
 const mountToast = () => {
-    const toast = document.getElementById('qbc-toast');
-    render(
-      <Toast
-        imagesStorageUrl={toast!.dataset.imagesStorageUrl!}
-      />,
-      toast
-    );
-  };
-  
-  export default mountToast;
-  
+  const toast = document.getElementById(id);
+  render(
+    <Toast id={id} imagesStorageUrl={toast!.dataset.imagesStorageUrl!} />,
+    toast
+  );
+};
+
+const showToast = (
+  message: string,
+  textColorClass: string,
+  timeToLive: number
+) => {
+  const toast = $(`#${id}-component`);
+  toast.data("message", message);
+  toast.data("text-color-class", textColorClass);
+  toast.data("delay", timeToLive);
+  toast.toast("show");
+  console.log("show toast");
+};
+
+export { mountToast, showToast };
