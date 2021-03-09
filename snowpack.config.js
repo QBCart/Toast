7fd1b3b84926a5ccac3440c8480c55bc6e74c655
Mshea424 <61366103+Mshea424@users.noreply.github.com>
2021-03-09 11:50:10 -0500

    add

---
+++ snowpack.config.js
@@ -0,0 +1,23 @@
+// Snowpack Configuration File
+// See all supported options: https://www.snowpack.dev/reference/configuration
+
+/** @type {import("snowpack").SnowpackUserConfig } */
+module.exports = {
+    mount: {
+      src: { url: "/", resolve: true, static: false }
+    },
+    plugins: [],
+    packageOptions: {
+      source: "remote",
+      types: true,
+      
+    },
+    devOptions: {},
+    buildOptions: {},
+    optimize: {
+      bundle: false,
+      minify: false,
+      target: 'es2020'
+    }
+  };
+  
