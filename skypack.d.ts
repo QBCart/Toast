85f4233648724c58c37cc3aff0dade3e6c09abd8
Mshea424 <61366103+Mshea424@users.noreply.github.com>
2021-03-09 12:57:49 -0500

    added dependencies

--- skypack.d.ts
+++ skypack.d.ts
@@ -1,7 +1,7 @@
 declare module 'https://cdn.skypack.dev/pin/react@v17.0.1-tOtrZxBRexARODgO0jli/min/react.js' {
-    export { useEffect } from 'react';
+  export { FC, useState, useEffect } from 'react';
 }
 
 declare module 'https://cdn.skypack.dev/pin/react-dom@v17.0.1-DtIXT56q6U8PbgLMrBhE/min/react-dom.js' {
-  export * from 'react-dom';
+  export { render } from 'react-dom';
 }
