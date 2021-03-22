ab301db96a40eef29d92cf89c400d3667f51bb55
Mshea424 <61366103+Mshea424@users.noreply.github.com>
2021-03-22 11:46:55 -0400

    add skypack deps

--- skypack.d.ts
+++ skypack.d.ts
@@ -1,7 +1,4 @@
-declare module 'https://cdn.skypack.dev/pin/react@v17.0.1-tOtrZxBRexARODgO0jli/min/react.js' {
-  export { FC, useState, useEffect } from 'react';
-}
-
-declare module 'https://cdn.skypack.dev/pin/react-dom@v17.0.1-DtIXT56q6U8PbgLMrBhE/min/react-dom.js' {
-  export { render } from 'react-dom';
+declare module 'https://www.skypack.dev/view/@qbcart/eshop-skypack' {
+  export { React } from '@qbcart/eshop-skypack/react';
+  export { render } from '@qbcart/eshop-skypack/react-dom';
 }
