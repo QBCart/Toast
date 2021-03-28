f80d8c6726cf4dedf1b851721c7b0db002d55820
Allan Mobley <allan.mobley.jr@mobsites.com>
2021-03-27 22:09:20 -0400

    updated copy

--- src/styled-components/styled-toast-header.ts
+++ src/styled-components/styled-toast-header.ts
@@ -1,3 +1,11 @@
+/**
+ * @license
+ * Copyright (c) 2021 QBCart Inc. All rights reserved.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source repo.
+ */
+
 import styled from 'styled-components';
 
 const StyledToastHeader = styled.div`
