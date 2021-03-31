9d5e182e2aec878ed0e7c4eb40ca0ad3f704847d
Allan Mobley <allan.mobley.jr@mobsites.com>
2021-03-31 07:58:00 -0400

    support icons

---
+++ src/styled-components/styled-toast-icon.ts
@@ -0,0 +1,19 @@
+/**
+ * @license
+ * Copyright (c) 2021 QBCart Inc. All rights reserved.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source repo.
+ */
+
+import styled from 'styled-components';
+
+const StyledToastIcon = styled.div`
+  height: 18px;
+  width: 18px;
+  font-size: 0.875rem;
+  margin-right: 5px;
+  margin-bottom: 5px;
+`;
+
+export default StyledToastIcon;
