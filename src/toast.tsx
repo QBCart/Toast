3d5aebfc7f8dbe7ef7dea5edb7da6cd9c6f526ee
Allan Mobley <allan.mobley.jr@qbcart.net>
2021-04-07 09:13:11 +0000

    refactor and fix logo disappear bug

--- src/toast.tsx
+++ src/toast.tsx
@@ -12,6 +12,7 @@ import StyledToast from './styled-components/styled-toast.js';
 import StyledToastBody from './styled-components/styled-toast-body.js';
 import StyledToastHeader from './styled-components/styled-toast-header.js';
 import StyledToastIcon from './styled-components/styled-toast-icon.js';
+import type { CSSProperties } from 'styled-components';
 
 interface Props {
   id: string;
@@ -23,6 +24,9 @@ const Toast: FC<Props> = (props: Props) => {
   const alert = useToastAlert();
   const removeAlert = useRemoveToastAlert();
 
+  /*
+   *  Animation must be set in useEffect to function properly.
+   */
   useEffect(() => {
     if (alert) {
       const toast = ref.current!;
@@ -30,26 +34,12 @@ const Toast: FC<Props> = (props: Props) => {
       toast.style.animationDuration = `${
         (alert.duration ?? 0) > 0 ? alert.duration : 2
       }s`;
-      toast.style.backgroundColor =
-        alert.bodyBackgroundColor || 'rgba(255, 255, 255, 0.85)';
-      toast.style.color = alert.bodyTextColor || 'black';
-
-      const header = toast.querySelector<HTMLDivElement>(
-        `#${props.id}-header`
-      )!;
-      header.style.color = alert.headerTextColor || '#6c757d';
-      header.style.backgroundColor =
-        alert.headerBackgroundColor || 'rgba(255, 255, 255, 0.85)';
-
-      if (alert.iconName) {
-        const icon = toast.querySelector<HTMLDivElement>(`#${props.id}-icon`)!;
-        icon.innerHTML = `<span class="material-icons" style="margin-top: -1px; color: ${
-          alert.iconColor || '#6c757d'
-        }">${alert.iconName}</span>`;
-      }
     }
   }, [alert, props, ref]);
 
+  /*
+   *  Animation must be unset onAnimationEnd to function properly.
+   */
   const onAnimationEnd = async (): Promise<void> => {
     const toast = ref.current!;
     toast.style.animationName = '';
@@ -59,21 +49,43 @@ const Toast: FC<Props> = (props: Props) => {
     }
   };
 
+  const toastStyle: CSSProperties = {
+    color: alert?.bodyTextColor || 'black',
+    backgroundColor: alert?.bodyBackgroundColor || 'rgba(255, 255, 255, 0.85)'
+  };
+
+  const headerStyle: CSSProperties = {
+    color: alert?.headerTextColor || '#6c757d',
+    backgroundColor: alert?.headerBackgroundColor || 'rgba(255, 255, 255, 0.85)'
+  };
+
+  const iconStyle: CSSProperties = {
+    marginTop: '-1px',
+    color: alert?.iconColor || '#6c757d'
+  };
+
   return (
     <StyledToast
       ref={ref}
+      style={toastStyle}
       role="alert"
       aria-live="assertive"
       aria-atomic="true"
       onAnimationEnd={() => onAnimationEnd()}
     >
-      <StyledToastHeader id={`${props.id}-header`}>
-        <StyledToastIcon id={`${props.id}-icon`}>
-          <img
-            src={`${props.imagesStorageUrl}images/favicon.ico`}
-            alt="company logo"
-            width="18"
-          />
+      <StyledToastHeader style={headerStyle}>
+        <StyledToastIcon>
+          {alert?.iconName ? (
+            <span className="material-icons" style={iconStyle}>
+              {alert.iconName}
+            </span>
+          ) : (
+            <img
+              src={`${props.imagesStorageUrl}images/favicon.ico`}
+              alt="company logo"
+              width="18"
+            />
+          )}
         </StyledToastIcon>
 
         <strong className="ml-1 mr-auto">{alert?.headerText}</strong>
